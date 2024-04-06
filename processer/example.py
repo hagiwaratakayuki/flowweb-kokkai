fd4638ae6a650d7b3b5dc4cb16ce46dd718ec57f
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2024-04-06 17:04:48 +0900

    delete nonuse file

--- processer/example.py
+++
@@ -1,9 +0,0 @@
-from doc2vec.vectaizer.gensim_fasttext import loadVectors
-
-vec = loadVectors()
-count = 0
-for k in vec.key_to_index:
-    print(type(k))
-    count +=1 
-    if count > 100:
-        break
