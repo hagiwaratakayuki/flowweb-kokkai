4840e69c7649e10fed7145ec47af3f6862b938d8
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2024-12-14 07:06:38 +0900

    update valid noun logic

--- processer/doc2vec/components/japanese_language/rule/valid_noun_jp.py
+++ processer/doc2vec/components/japanese_language/rule/valid_noun_jp.py
@@ -1,8 +1,8 @@
 import regex as re
 from doc2vec.components.japanese_language.regex_patterns import hiragana_include
 from doc2vec.components.japanese_language.regex_patterns import noun_blockpattern
-# MeCabとデフォルト辞書の旧字体及び「つ」カウント(一つとか)、話し言葉の間投詞的(皆様、等々など)表現対策
+# MeCabとデフォルト辞書の旧字体及び「つ」カウント(一つとか)、話し言葉の間投詞的(皆様、等々など)表現対策 + 子ども(子供・こども)の統一
 
 
 def check_valid_noun(face: str):
-    return hiragana_include.compiled.search(face) is None and noun_blockpattern.compiled.search(face) is None
+    return face == "子ども" or (hiragana_include.compiled.search(face) is None and noun_blockpattern.compiled.search(face) is None)
