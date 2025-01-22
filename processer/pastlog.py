c198095cd6a6efe272958cc1b5c3814c0a3ba4f5
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2025-01-23 08:03:23 +0900

    update pastlog processer

--- processer/pastlog.py
+++ processer/pastlog.py
@@ -3,6 +3,8 @@ from storage import basic as storage
 import syslog
 import traceback
 from execute_logics import kokkai
+import subprocess
+
 
 storage.set_location(LOCATION)
 storage.set_project_id(PROJECT_ID)
@@ -10,3 +12,6 @@ try:
     kokkai.execute()
 except Exception as e:
     syslog.syslog(r''.join(traceback.format_exception(e)))
+
+
+subprocess.run(["shutdown", "-h", "now"])
