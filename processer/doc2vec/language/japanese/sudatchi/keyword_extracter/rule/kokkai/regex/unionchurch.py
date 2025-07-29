914a4499934e0c8f083a03fc83571ad5e19d8a84
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2025-07-29 11:58:05 +0900

    統一教会と桜を見る会のルール実装

---
+++ processer/doc2vec/language/japanese/sudatchi/keyword_extracter/rule/kokkai/regex/unionchurch.py
@@ -0,0 +1,6 @@
+import re
+from processer.doc2vec.language.japanese.sudatchi.keyword_extracter.rule.component.regex import SudatchiRegexRule
+
+
+rule = SudatchiRegexRule(re.compile(
+    '旧?世界基督教統一神霊協会|旧?統一教会|旧?統一協会|世界平和統一家庭連合|(?<!世界平和統一)家庭連合'), output_word=['統一協会', '統一教会', '世界平和統一家庭連合', '家庭連合'], is_bind_output=True)
