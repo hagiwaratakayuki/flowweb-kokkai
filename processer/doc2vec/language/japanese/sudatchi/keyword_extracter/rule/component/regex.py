c8f698abfe6db9979dd9a2944a412da9f10a5fa6
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2025-07-29 11:05:51 +0900

    fix

--- processer/doc2vec/language/japanese/sudatchi/keyword_extracter/rule/component/regex.py
+++ processer/doc2vec/language/japanese/sudatchi/keyword_extracter/rule/component/regex.py
@@ -1,8 +1,15 @@
+from functools import reduce
+from typing import Set
+
 from processer.doc2vec.base.keyword_extracter.rule.regex import RegexRule
 from processer.doc2vec.language.japanese.sudatchi.tokenizer.dto import SudatchiDTO
 from processer.doc2vec.language.japanese.sudatchi.util.token_search import token_searcher
 
 
+def reducer(a: Set, b: Set):
+    return a | b
+
+
 class SudatchiRegexRule(RegexRule):
     def _search_tokens(self, parse_result, matches):
-        return set(token_searcher.search(matches=matches, parse_result=parse_result).keys())
+        return reduce(reducer, token_searcher.search(matches=matches, parse_result=parse_result).values(), set)
